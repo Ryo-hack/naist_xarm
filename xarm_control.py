a77df7ff3e8283fdc85683ef258d3d59d59eb3a2
ryo-h@ck <56574063+Ryo-hack@users.noreply.github.com>
2022-11-25 05:10:55 +0900

    Update xarm_control.py

--- xarm_control.py
+++ xarm_control.py
@@ -1,8 +1,14 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import rospy as rp
 import numpy as np
+import math, time
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+
 from xarm.wrapper import XArmAPI
 
 class xarm:
@@ -14,28 +20,46 @@ class xarm:
 
         self._arm.reset(wait=True)
 
-    def _set_position(self, x, y, z, roll, pitch, yaw, speed=1000):
-        self._arm.set_position(x=x, y=y, z=z, roll=roll, pitch=pitch, yaw=yaw, speed=speed, wait=True)
+    def _offset_deg(self,x,y,z):
+        offset_deg = 50
+        alt_x=x*math.cos(offset_deg*(math.pi/180)) + y*math.sin(offset_deg*(math.pi/180))
+        alt_y=-(x*math.sin(offset_deg*(math.pi/180)) - y*math.cos(offset_deg*(math.pi/180)))
+        alt_z=z
+        return alt_x, alt_y, alt_z
+     
+    def _set_position(self, x, y, z, roll, pitch, yaw, speed=50):
+        ox, oy, oz = self._offset_deg(x,y,z)
+        self._arm.set_tool_position(x=ox, y=oy, z=oz, roll=roll, pitch=pitch, yaw=yaw, speed=speed, wait=True)
 
     def _random_pos(self):
-        x = 300
+        x = 700
         y = np.random.randint(-700, 700)
         z = np.random.randint(100, 967)
-
-        self._set_position(x,y,z,0,0,0)
-        self._arm.set_tool_position(0,0,0,90,0,0)
+        try:
+            # x:300, y:0, z:-300, roll:0, pitch:180, yaw:0
+            # self._set_position(0,0,-300,0,180,0)
+            print(self._arm.get_position())
+        except self._arm.error_code:
+            print('error')
+            self._reset()
+        # self._arm.set_tool_position(0,0,0,90,0,0)
 
 
     def _reset(self):
         self._arm.reset(wait=True)
+        self._arm.disconnect()
 
 def main():
+    
     try:
-        arm = xarm()
+    #     time.sleep(2)
+        arm = xarm('192.168.1.213')
         arm._random_pos()
+        arm._reset()
         
     except rp.ROSInterruptException:
+        print('yo')
         pass
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     main()
